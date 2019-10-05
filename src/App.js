e589d5910e282925e21a371ef53151450fc52b7a
SLO-MO-User <tuncel.halit@hotmail.com>
2019-10-05 16:09:52 +0300

    Basic weather app using openweathermap api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+
+  const [weatherFetchError, setWeatherFetchError] = useState(undefined);
+
+  const [weatherInfo, setWeatherInfo] = useState({
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+  });
+
+  const { temperature, city, country, humidity, description } = weatherInfo;
+
+  const getWeather = async e => {
+    e.preventDefault();
+    try {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${searchText}&units=metric&lang=tr&appid=${process.env.REACT_APP_API_KEY}`
+      );
+      const data = await api_call.json();
+      console.log(data);
+      setWeatherInfo({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+      });
+    } catch (error) {
+      setWeatherFetchError(error);
+      console.log(weatherFetchError);
+    }
+  };
+
+  const onSearchInputCange = e => {
+    setSearchText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='col-12' style={{ height: '100vh', background: 'black' }}>
+      <Header />
+      <div className='col-12 mt-5'>
+        <div className='col-12 col-md-8 offset-md-2 mt-5 mb-5'>
+          <form onSubmit={getWeather}>
+            <div className='input-group'>
+              <input
+                type='text'
+                placeholder='Search City'
+                className='form-control text-dark border-success'
+                value={searchText}
+                onChange={onSearchInputCange}
+              />
+
+              <div className='input-group-append'>
+                <button type='submit' className='btn btn-outline-success'>
+                  Get Weather
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className='col-12 col-md-8 offset-md-2'>
+          <div className='card border border-success'>
+            <div className='card-body' style={{ background: 'black' }}>
+              <ul className='list-group list-group-flush'>
+                <li
+                  className='list-group-item d-flex justify-content-between text-white'
+                  style={{ background: 'black' }}
+                >
+                  Şehir: <span className='text-success'> {city} </span>
+                </li>
+                <li
+                  className='list-group-item d-flex justify-content-between text-white'
+                  style={{ background: 'black' }}
+                >
+                  Sıcaklık:
+                  <span className='text-success'> {temperature} &#8451; </span>
+                </li>
+                <li
+                  className='list-group-item d-flex justify-content-between text-white'
+                  style={{ background: 'black' }}
+                >
+                  Ülke: <span className='text-success'> {country} </span>
+                </li>
+                <li
+                  className='list-group-item d-flex justify-content-between text-white'
+                  style={{ background: 'black' }}
+                >
+                  Nem: <span className='text-success'> {humidity} </span>
+                </li>
+                <li
+                  className='list-group-item d-flex justify-content-between text-white'
+                  style={{ background: 'black' }}
+                >
+                  Açıklama:
+                  <span
+                    className='text-success text-truncate'
+                    title={description}
+                  >
+                    {description ? description.toUpperCase() : ''}
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <ul className='nav justify-content-center p-1 border-bottom border-success'>
+      <li className='nav-item'>
+        <span className='h4 nav-link active text-success'>
+          Weather Application
+        </span>
+      </li>
+    </ul>
+  );
+}
+
+export default Header;
